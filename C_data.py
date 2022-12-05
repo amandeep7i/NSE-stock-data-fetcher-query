d8fe408fd36118ced39bf4924a53a786906349d5
Aman Kumar <adeepraghuvanshi@gmail.com>
2022-12-05 12:22:49 +0530

    second commit(All the Queries RUnning)

--- C_data.py
+++ C_data.py
@@ -1,5 +1,7 @@
 import pandas 
 import sqlite3
+import time
+# from datetime import timedeltas_date2
 
 # First Query
 df = pandas.read_csv("New_File.csv")
@@ -11,42 +13,58 @@ df.to_sql(table_name, conn, if_exists='append', index=False)
 crsr = conn.cursor()
 # SQL command to create a table in the database
 # execute the statement
-crsr.execute('''SELECT ((CLOSE-OPEN)/(OPEN)) AS Gain FROM MY_table ORDER BY (Gain) DESC limit 25''')
+crsr.execute('''SELECT `NAME OF COMPANY`, ((CLOSE-OPEN)/(OPEN)) AS Gain FROM MY_table ORDER BY (Gain) DESC limit 25''')
 data = crsr.fetchall()
 print("TOP 25 QUERIES ARE AS FOLLOWS :")
 for d in data:
-    print(d[0],end="\n")
+    print(d,end="\n")
 # close the connection
 conn.close()
 
 
 # Second QUery
 # Get datewise top 25 gainers for last 30 days as per point 4 above.
-# for i in range(30):
-#     df = pandas.read_csv("bahvcopies"+"f{i}"+".csv")
-#     table_name = "MY_table"
-#     f = open("real"+"f{i}"+".db", "w")
-#     conn = sqlite3.connect("real"+"f{i}"+".db")
-#     df.to_sql(table_name, conn, if_exists='append', index=False)
+count =1
+for i in range(30):
+    df = pandas.read_csv('bhavcopies'+f"{count}"+'.csv')
+    column = df["TIMESTAMP"]
+    print(column[0])
+    merged = pandas.read_csv('bhavcopies'+f"{count}"+'.csv')
+    merged.to_csv('New_File'+f"{count}"+'.csv')
+    df = pandas.read_csv('D:/program/INTERN_ASSIGNMENT'+'/New_File'+f"{count}"+'.csv')
+    table_name = "MY_table"
+    f = open('real'+f"{count}"+'.db', "w")
+    conn = sqlite3.connect('real'+f"{count}"+'.db')
+    df.to_sql(table_name, conn, if_exists='append', index=False)
 
-#     crsr = conn.cursor()
-#     crsr.execute('''SELECT ((CLOSE-OPEN)/(OPEN)) AS Gain FROM MY_table ORDER BY (Gain) DESC limit 25''')
-#     data = crsr.fetchall()
-#     print("TOP 25 QUERIES ARE AS FOLLOWS :")
-#     for d in data:
-#         print(d[0],end="\n")
+    crsr = conn.cursor()
+    crsr.execute('''SELECT SYMBOL, ((CLOSE-OPEN)/(OPEN)) AS Gain FROM MY_table ORDER BY (Gain) DESC limit 25''')
+    data = crsr.fetchall()
+    print("TOP 25 QUERIES ARE AS FOLLOWS :")
+    for d in data:
+        print(d,end="\n")
+    count+=1
+    print()
+    time.sleep(1)
+    # print(count)
 
 
-# #3rd Query
-# # 3. Alternatively, you can also get a single list of the top 25 gainers based on open of the oldest day and close of latest day of those 30 days as per point 4.
-# df = pandas.read_csv("bahvcopies0.csv")
-# table_name = "MY_table0"
-# f = open("real0.db", "w")
-# conn = sqlite3.connect("real0.db")
-# df.to_sql(table_name, conn, if_exists='append', index=False)
-# crsr = conn.cursor()
-# crsr.execute('''SELECT ((CLOSE-OPEN)/(OPEN)) AS Gain FROM MY_table ORDER BY (Gain) DESC limit 25''')
-# data = crsr.fetchall()
-# print("TOP 25 QUERIES ARE AS FOLLOWS :")
-# for d in data:
-#     print(d[0],end="\n")
+#3rd Query
+# 3. Alternatively, you can also get a single list of the top 25 gainers based on open of the oldest day and close of latest day of those 30 days as per point 4.
+# df = pandas.read_csv("bahvcopies1.csv")
+# df.to_csv('New_File'+f"{count}"+'.csv')
+table_name1 = "MY_table1"
+table_name2 = "MY_table2"
+
+df1 = pandas.read_csv('bhavcopies1.csv')
+df2 = pandas.read_csv('bhavcopies1.csv')
+f = open("real_new.db", "w")
+conn = sqlite3.connect("real_new.db")
+df1.to_sql(table_name1, conn, if_exists='append', index=False)
+crsr = conn.cursor()
+df2.to_sql(table_name2, conn, if_exists='append', index=False)
+crsr.execute('''SELECT MY_table1.SYMBOL, ((MY_table1.CLOSE-MY_table2.OPEN)/(MY_table2.OPEN)) AS Gain FROM MY_table1 FULL OUTER JOIN MY_table2 on MY_table1.OPEN = MY_table2.CLOSE ORDER BY (Gain) DESC limit 25''')
+data = crsr.fetchall()
+print("TOP 25 QUERIES ARE AS FOLLOWS Based on open of the oldest day and close of the latest day:")
+for d in data:
+    print(d,end="\n")
--- Fetch_data.py
+++ Fetch_data.py
@@ -40,50 +40,78 @@ def get_month(m):
 url1 = 'https://archives.nseindia.com/content/equities/EQUITY_L.csv'
 df = pd.read_csv(url1)
 df.to_csv('fSecurties.csv')
-
+print("Program 1 FInished!!")
 
 # 2. Programatically get the latest “bhavcopy” csv file
 today = datetime.datetime.now()
+today = today -timedelta(days=1)
 yr = str(today.year)
 m = today.month
 month=get_month(m)
-d = str(today.day)
-# print(d)
-# print(f"{d}{month}{yr}")
+# print(today.day)
+d = '{:02d}'.format(today.day)
+d = str(d)
 current_date = d+month+yr
-url2 = 'https://archives.nseindia.com/content/historical/EQUITIES/2022/NOV/'+'cm'+f"{current_date}"+'bhav.csv.zip'
-df = pd.read_csv(url2)
-df.to_csv('bhavcopies.csv')
-
-
-
+# print(today)
+url2 = 'https://archives.nseindia.com/content/historical/EQUITIES/2022/'+f"{month}"+'/cm'+f"{current_date}"+'bhav.csv.zip'
+r = requests.head(url2)
+if r.status_code == 503:
+    # print(f'{url2} was found')
+    df = pd.read_csv(url2)
+    df.to_csv('bhavcopies.csv')
+# https://archives.nseindia.com/content/historical/EQUITIES/2022/DEC/cm02DEC2022bhav.csv.zip
+print("Program 2 FInished!!")
 # load the data into a Pandas DataFrame
 merged = pd.read_csv('bhavcopies.csv').merge(pd.read_csv('fSecurties.csv'),on=["SYMBOL"])
 merged.drop("Unnamed: 0_x",inplace=True,axis=1)
 merged.drop("Unnamed: 0_y",inplace=True,axis=1)
 merged.drop("Unnamed: 13",inplace=True,axis=1)
 merged.to_csv('New_File.csv')
-
+print("Program 3 FInished!!")
 
 
 # 4. In addition to step 2, programmatically get BHAvcopies of the last 30 days instead of just the latest one.
 today1 = datetime.datetime.now()
+today1 = today1 - timedelta(days=1) 
+# DOne as 3dec file is still not available
 dt = datetime.datetime.now()
-
-for i in range(30):
+dt = dt - timedelta(days=1) 
+count = 1
+i = 0
+while 1:
     yesterday = today1 - timedelta(days=i)
     dt = today - timedelta(days=i)
+    i+=1
+    
     x = dt.weekday()
+    # print(x)
     yr = str(yesterday.year)
     m = yesterday.month
     month=get_month(m)
-    d = str(yesterday.day)
+    d = '{:02d}'.format(yesterday.day)
+    d = str(d)
     if x==5 or x==6:   
         continue
-    if d=='0':
+    if d=='00':
         continue
     current_date = d+month+yr
-    print(current_date)
-    url2 = 'https://archives.nseindia.com/content/historical/EQUITIES/2022/NOV/'+'cm'+f"{current_date}"+'bhav.csv.zip'
-    df = pd.read_csv(url2)
-    df.to_csv('bhavcopies'+f"{i}"+'.csv')
+    if current_date == '08NOV2022' :
+        continue
+    if current_date == '26OCT2022' :
+        continue 
+    # print(current_date)
+    
+    url2 = 'https://archives.nseindia.com/content/historical/EQUITIES/2022/'+f"{month}"+'/cm'+f"{current_date}"+'bhav.csv.zip'
+    r = requests.get(url2)
+    # print(url2)
+    # print(r.status_code)
+    if r.status_code == 200:
+        # print(f'{url2} was found')
+        df = pd.read_csv(url2)
+        count+=1
+        df.to_csv('bhavcopies'+f"{count}"+'.csv')
+        
+    if count ==30:
+        break
+    # print(count)
+print("Program 4 FInished!!")    
